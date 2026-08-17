5c3535f74dd7246938101a8155c273ac1bf6a911
chkf <13533309331@163.com>
2026-08-17 17:36:09 +0800

    。

--- reference/1/assignment1-basics/src/tokenization/_types.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-type PreToken = bytes
-type Chunk = bytes
-type PreTokenCount = defaultdict[PreToken, int]
-type Token = bytes
-type Vocab = dict[int, Token]
-type PairCount = defaultdict[tuple[Token, Token], int]
--- reference/Stanford-CS336-main/assignment5-alignment/cs336_alignment/algs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ei import EITrainConfig, EITrainer
-from .grpo import GRPOTrainConfig, GRPOTrainer
-from .sft import SFTTrainer, SFTTrainingConfig
-
-__all__ = ["SFTTrainer", "SFTTrainingConfig", "EITrainer", "EITrainConfig", "GRPOTrainer", "GRPOTrainConfig"]
